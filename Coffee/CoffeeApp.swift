994af9bb49fac82a26eeb166d2aca8eec11d0677
pmackley <130097925+pmackley@users.noreply.github.com>
2023-05-31 16:00:31 -0500

    Initial Commit

---
+++ Coffee/CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp.swift
+//  Coffee
+//
+//  Created by Parker Revers on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
